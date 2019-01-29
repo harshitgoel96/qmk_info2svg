fc64d2628e093314b9226fce010ed4fb724a4319
Harshit Goel <harshitgoel96@yahoo.com>
2019-01-29 21:36:43 +0530

    added label

--- info2svg.py
+++ info2svg.py
@@ -5,8 +5,8 @@ import json
 BASE_KEY_WIDTH=19.05
 BASE_KEY_HEIGHT=19.05
 
-def createSvgForLayout(layoutName,layoutArray):
-    dwg = svgwrite.Drawing("{}.svg".format(layoutName))
+def createSvgForLayout(layoutName,layoutArray,sizeX,sizeY):
+    dwg = svgwrite.Drawing("{}.svg".format(layoutName),size=('{}mm'.format(sizeX*BASE_KEY_WIDTH), '{}mm'.format(sizeY*BASE_KEY_HEIGHT)), viewBox=('0 0 {} {}'.format(sizeX*BASE_KEY_WIDTH,sizeY*BASE_KEY_HEIGHT)))
     defaultRx=0
     defaultRy=0
     for keyItem in layoutArray:
@@ -15,20 +15,37 @@ def createSvgForLayout(layoutName,layoutArray):
         keyRotation=keyItem.get("r",0)
         defaultRx=keyItem.get("rx",defaultRx)
         defaultRy=keyItem.get("ry",defaultRy)
+        stdLayout=keyItem.get("label","A")
         if keyRotation!=0:
            dwg.add(dwg.rect((keyItem["x"]*BASE_KEY_WIDTH,keyItem["y"]*BASE_KEY_HEIGHT),(keyItemWidthMultiplier*BASE_KEY_WIDTH,keyItemHeighthMultiplier*BASE_KEY_HEIGHT), 
-                stroke='red', 
-                stroke_width=1,
+                stroke='gray',fill="white" ,
+                stroke_width=0.5,
                 transform="rotate({} {} {})".format(
                         keyRotation,
                         defaultRx*BASE_KEY_WIDTH,
                         defaultRy*BASE_KEY_HEIGHT)
            ))
-           
+           dwg.add(dwg.text(stdLayout,
+                insert=((keyItem["x"]*BASE_KEY_WIDTH)+BASE_KEY_WIDTH/10,
+                (keyItem["y"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH*0.9)),
+                
+                transform="rotate({} {} {})".format(
+                        keyRotation,
+                        defaultRx*BASE_KEY_WIDTH,
+                        defaultRy*BASE_KEY_HEIGHT)))
         else:
             dwg.add(dwg.rect(
                 (keyItem["x"]*BASE_KEY_WIDTH,keyItem["y"]*BASE_KEY_HEIGHT),
-                (keyItemWidthMultiplier*BASE_KEY_WIDTH,keyItemHeighthMultiplier*BASE_KEY_HEIGHT), stroke='red', stroke_width=1))
+                (keyItemWidthMultiplier*BASE_KEY_WIDTH,keyItemHeighthMultiplier*BASE_KEY_HEIGHT),  stroke='gray',fill="white" ,
+                stroke_width=0.5,))
+            dwg.add(
+                dwg.text(
+                    stdLayout,
+                    insert=((keyItem["x"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH/10),
+                    (keyItem["y"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH*0.9))
+                    )
+                )
+        
     dwg.save()
 #connection = http.client.HTTPSConnection("api.qmk.fm")
 #connection.request("GET", "/v1/keyboards/ergodone")
@@ -45,6 +62,6 @@ for key in listOfKeys:
     keebLayouts=data["keyboards"][key]["layouts"].keys()  
     for layout in keebLayouts:
         #print("===========\n{}\n{}\n".format(layout,data["keyboards"][key]["layouts"][layout]['layout']))
-        createSvgForLayout(layout,data["keyboards"][key]["layouts"][layout]['layout'])
+        createSvgForLayout(layout,data["keyboards"][key]["layouts"][layout]['layout'],data["keyboards"][key]["width"],data["keyboards"][key]["height"])
     
 print("end")
