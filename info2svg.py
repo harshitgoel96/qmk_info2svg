330443407539cf2608e6b580a5d677e5e702d154
Goel <HGoel9@INTERNAL.COLT.NET>
2019-02-01 11:16:24 +0530

    added margins

--- info2svg.py
+++ info2svg.py
@@ -2,11 +2,17 @@ import svgwrite
 import http.client
 import json 
 
-BASE_KEY_WIDTH=19.05
-BASE_KEY_HEIGHT=19.05
+#define 1u value
+BASE_KEY_UNIT=20
+
+#define key margin
+BASE_KEY_MARGIN=4
+
+#should rotation values be considered or not
+ROTATION_ENABLE=False
 
 def createSvgForLayout(layoutName,layoutArray,sizeX,sizeY):
-    dwg = svgwrite.Drawing("{}.svg".format(layoutName),size=('{}mm'.format(sizeX*BASE_KEY_WIDTH), '{}mm'.format(sizeY*BASE_KEY_HEIGHT)), viewBox=('0 0 {} {}'.format(sizeX*BASE_KEY_WIDTH,sizeY*BASE_KEY_HEIGHT)))
+    dwg = svgwrite.Drawing("{}.svg".format(layoutName),size=('{}mm'.format(sizeX*BASE_KEY_UNIT), '{}mm'.format(sizeY*BASE_KEY_UNIT)), viewBox=('0 0 {} {}'.format(sizeX*BASE_KEY_UNIT,sizeY*BASE_KEY_UNIT)))
     defaultRx=0
     defaultRy=0
     for keyItem in layoutArray:
@@ -16,33 +22,33 @@ def createSvgForLayout(layoutName,layoutArray,sizeX,sizeY):
         defaultRx=keyItem.get("rx",defaultRx)
         defaultRy=keyItem.get("ry",defaultRy)
         stdLayout=keyItem.get("label","A")
-        if keyRotation!=0:
-           dwg.add(dwg.rect((keyItem["x"]*BASE_KEY_WIDTH,keyItem["y"]*BASE_KEY_HEIGHT),(keyItemWidthMultiplier*BASE_KEY_WIDTH,keyItemHeighthMultiplier*BASE_KEY_HEIGHT), 
+        if keyRotation!=0 and ROTATION_ENABLE:
+           dwg.add(dwg.rect((keyItem["x"]*BASE_KEY_UNIT+BASE_KEY_MARGIN/2,keyItem["y"]*BASE_KEY_UNIT+BASE_KEY_MARGIN/2),(keyItemWidthMultiplier*BASE_KEY_UNIT-BASE_KEY_MARGIN/2,keyItemHeighthMultiplier*BASE_KEY_UNIT-BASE_KEY_MARGIN/2), 
                 stroke='gray',fill="white" ,
                 stroke_width=0.5,
                 transform="rotate({} {} {})".format(
                         keyRotation,
-                        defaultRx*BASE_KEY_WIDTH,
-                        defaultRy*BASE_KEY_HEIGHT)
+                        defaultRx*BASE_KEY_UNIT,
+                        defaultRy*BASE_KEY_UNIT)
            ))
            dwg.add(dwg.text(stdLayout,
-                insert=((keyItem["x"]*BASE_KEY_WIDTH)+BASE_KEY_WIDTH/10,
-                (keyItem["y"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH*0.9)),
+                insert=((keyItem["x"]*BASE_KEY_UNIT)+BASE_KEY_UNIT/10,
+                (keyItem["y"]*BASE_KEY_UNIT)+(BASE_KEY_UNIT*0.9)),
                 
                 transform="rotate({} {} {})".format(
                         keyRotation,
-                        defaultRx*BASE_KEY_WIDTH,
-                        defaultRy*BASE_KEY_HEIGHT)))
+                        defaultRx*BASE_KEY_UNIT,
+                        defaultRy*BASE_KEY_UNIT)))
         else:
             dwg.add(dwg.rect(
-                (keyItem["x"]*BASE_KEY_WIDTH,keyItem["y"]*BASE_KEY_HEIGHT),
-                (keyItemWidthMultiplier*BASE_KEY_WIDTH,keyItemHeighthMultiplier*BASE_KEY_HEIGHT),  stroke='gray',fill="white" ,
+                (keyItem["x"]*BASE_KEY_UNIT+BASE_KEY_MARGIN/2,keyItem["y"]*BASE_KEY_UNIT+BASE_KEY_MARGIN/2),
+                (keyItemWidthMultiplier*BASE_KEY_UNIT-BASE_KEY_MARGIN/2,keyItemHeighthMultiplier*BASE_KEY_UNIT-BASE_KEY_MARGIN/2),  stroke='gray',fill="white" ,
                 stroke_width=0.5,))
             dwg.add(
                 dwg.text(
                     stdLayout,
-                    insert=((keyItem["x"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH/10),
-                    (keyItem["y"]*BASE_KEY_WIDTH)+(BASE_KEY_WIDTH*0.9))
+                    insert=((keyItem["x"]*BASE_KEY_UNIT)+(BASE_KEY_UNIT/10),
+                    (keyItem["y"]*BASE_KEY_UNIT)+(BASE_KEY_UNIT*0.9))
                     )
                 )
         
